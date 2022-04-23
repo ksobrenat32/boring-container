c3705c7a058b5ddc6988758beb6b84777e51295b
ksobrenat32 <ksobrenat32@quique.rocks>
2022-04-22 19:00:56 -0500

    Minecraft, Version and Type options The simple-mc container now has the option to choose between the vanilla server and paper server with plugins for bedrock. As minor changes: - More explicit documentation - Stopped using awk and now use pure jq - Added Labels to container - Follow apt container best practice

--- README.md
+++ README.md
@@ -10,25 +10,41 @@ Simple but useful containerfiles and guides,
 podman build -t image-name ./project/.
 ```
 
-## Simple java container prepared to run a minecraft server
+## Java container prepared to run a minecraft server
+
+This container is prepared for running a full
+ minecraft server, either if it existed before or
+ if it is brand new. In case you have an existing
+ minecraft world, it will try to run that.
+
+There are two types of server, vanilla (mojang) and
+ paper (community), the advantage of paper is that
+ it can be used with plugins in order to be able to
+ join using both java and bedrock clients!
 
 To run this container use:
 
 ```sh
 podman run -d \
     --name simple-mc \
-    -p 25565:25565 \
-    -p 19132:19132/udp \
-    -p 19133:19133/udp \
-    -e XMX=1024M `# Change to maximum ram for java` \
-    -e XMS=1024M `# Change to mimimum ram for java` \
-    -e SERVER_JAR=server.jar `# The name of the server executable` \
+    -p 25565:25565 `#Java port` \
+    -p 19132:19132/udp `#Only needed for paper with bedrock plugins` \
+    -p 19133:19133/udp `#Only needed for paper with bedrock plugins` \
+    -e XMX=1024M `#Change the maximum ram for java` \
+    -e XMS=1024M `#Change the mimimum ram for java` \
+    -e SERVER_JAR=server.jar `#The name of the server executable` \
+    -e SERVER_TYPE=VANILLA `#The type of server, VANILLA or PAPER` \
+    -e SERVER_VERSION=LATEST `# The version (ie. 1.14.3)` \
     -v /path/to/server/data:/data:z \
-    ghcr.io/ksobrenat32/simple-mc
+    ghcr.io/ksobrenat32/simple-mc:latest
 ```
 
+The container includes the mcrcon program, you can
+use it to connect to your server through the terminal
+after configuring the rcon service with:
+
 ```sh
-podman exec -it simple-mc mcrcon -H 127.0.0.1 -P <port> -p <password> -t
+podman exec -it containerName mcrcon -H 127.0.0.1 -P <port> -p <password> -t
 ```
 
 ## Jekyll container
--- minecraft/entrypoint.sh
+++ minecraft/entrypoint.sh
@@ -1,45 +1,76 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-JAVA_PATH="/usr/bin/java"
+set -eo pipefail
 
 # Running command.
+JAVA_PATH="/usr/bin/java"
 CMD="${JAVA_PATH} -Xmx${XMX} -Xms${XMS} -jar /data/${SERVER_JAR} nogui"
 
-if [ -z "$(ls -A /data)" ]; then
-    echo 
-    echo 'Downloading a new minecraft server'
-    echo "${CMD}"
-    echo
+if [[ -z "$(ls -A /data)" ]]; then
 
-    # Make sure to install needed tools
-    apt-get install -y -qq jq gawk
+    echo '
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|      No minecraft world detected      |
+|            Generating one             |
+|_______________________________________|
+'
 
-    # Getting the latest minecraft and paper version
-    LATEST_VERSION=$(curl -fsSL "https://launchermeta.mojang.com/mc/game/version_manifest.json" -H  "accept: application/json" | awk '{print $3}' | sed 's/\"//g' | sed 's/\,//g')
-    PAPER_VERSION=$(curl -fsSL "https://papermc.io/api/v2/projects/paper/versions/${LATEST_VERSION}" -H  "accept: application/json" | jq '.builds[-1]')
-    PAPER_URL="https://papermc.io/api/v2/projects/paper/versions/${LATEST_VERSION}/builds/${PAPER_VERSION}/downloads/paper-${LATEST_VERSION}-${PAPER_VERSION}.jar"
+    # VERSION
+    if [[ -z "${SERVER_VERSION}" ]] || [[ "${SERVER_VERSION}" == "LATEST" ]] ; then
+        VERSION=$(curl -fsSL "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r '.latest.release')
+        echo "Latest version selected by default, this is ${VERSION}"
+    else 
+        VERSION=${SERVER_VERSION}
+        echo "Trying with your selected version, this is ${VERSION}"
+    fi
 
-    # Downloading files
-    curl -fsSL ${PAPER_URL} -o /data/server.jar
-    mkdir -p /data/plugins
+    # SERVER_TYPE
+    case "$SERVER_TYPE" in
+        PAPER)
+            echo "Going with the Paper server and Bedrock plugins"
+            # Minecraft
+            PAPER_VERSION=$(curl -fsSL "https://papermc.io/api/v2/projects/paper/versions/${VERSION}" | jq '.builds[-1]')
+            SERVER_DOWNLOAD_URL="https://papermc.io/api/v2/projects/paper/versions/${VERSION}/builds/${PAPER_VERSION}/downloads/paper-${VERSION}-${PAPER_VERSION}.jar"
+
+            # Plugins
+            mkdir -p /data/plugins
     curl -fsSL "https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar" -o /data/plugins/Geyser-Spigot.jar
     curl -fsSL "https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar" -o /data/plugins/floodgate-spigot.jar
+            mkdir -p /data/plugins/Geyser-Spigot
+            mv /opt/mc/Geyser/config.yml /data/plugins/Geyser-Spigot/config.yml
+            ;;
+
+        VANILLA)
+            echo "Going with the Vanilla server"
+            VERSION_JSON=$(curl -fsSL "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r --arg VERSION_TARGET "${VERSION}" '.versions | .[] | select(.id==$VERSION_TARGET) | .url')
+            SERVER_DOWNLOAD_URL=$(curl -fsSL "${VERSION_JSON}" | jq -r '.downloads.server.url')
+            ;;
+        *)
+            echo "Select a valid server type, exiting ..."
+            exit 127
+            ;;
+    esac
+
+    # Download server
+    echo "
+    Downloading ${SERVER_TYPE} server, version ${VERSION} ...
+    URL =  ${SERVER_DOWNLOAD_URL}
+    "
+    curl -fsSL ${SERVER_DOWNLOAD_URL} -o /data/server.jar
 
     # Accept EULA
     echo "eula=true" >> /data/eula.txt
 
     # Try running
-    echo 
-    echo 'Trying to start the minecraft server running:'
-    echo "${CMD}"
-    echo
+    echo "
+Trying to start the minecraft server running:
+${CMD}
+"
 
 else
-    echo 
-    echo 'Trying to start the minecraft server running'
-    echo "${CMD}"
-    echo
+    echo "
+Trying to start the minecraft server running:
+${CMD}
+"
 fi
 
 # Run the command.
