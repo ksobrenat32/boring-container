9cca6285b836879b159187e441bdd6196990b1fd
ksobrenat32 <ksobrenatural@tuta.io>
2024-07-24 20:37:32 -0600

    move-movies: Manual script does not modify the original file

--- move-movies/manual.sh
+++ move-movies/manual.sh
@@ -1,27 +1,12 @@
 #! /usr/bin/env bash
 set -e
 
-# Rename _ to .
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/_/./g' {} +
-# Rename spaces to .
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/ /./g' {} +
-# Rename accents to normal characters
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/á/a/g' {} +
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/é/e/g' {} +
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/í/i/g' {} +
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/ó/o/g' {} +
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/ú/u/g' {} +
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/Á/A/g' {} +
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/É/E/g' {} +
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/Í/I/g' {} +
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/Ó/O/g' {} +
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's/Ú/U/g' {} +
-# Remove other special characters
-find ${ORIGIN} -type f ! -name "*.chunk*" -exec prename -v 's#[^[:alnum:]./]##g' {} +
+CUID=${CUID:-1000}
+ORIGIN=${ORIGIN:-/origin}
+DESTINY=${DESTINY:-/destiny}
 
-for FILE_NAME in $(ls --ignore='*.chunk*' ${ORIGIN} )
-do
-    FILE_NAME="${ORIGIN}/${FILE_NAME}"
+function move-movies(){
+    FILE_NAME=$1
     NAME="${FILE_NAME##*/}"
     NAME="${NAME%.*}"
     OUT_FILE=${DESTINY}/${NAME}.mp4
@@ -52,6 +37,29 @@ do
             "${OUT_FILE}"
     fi
 
-    chown ${CUID}:${CUID} ${OUT_FILE}
-    rm ${FILE_NAME}
-done
+    chown ${CUID}:${CUID} "${OUT_FILE}"
+}
+
+export -f move-movies
+
+find ${ORIGIN} -type f -exec bash -c 'move-movies "$0"' {} \;
+
+# Rename _ to .
+find ${DESTINY} -type f -exec prename -v 's/_/./g' {} +
+# Rename spaces to .
+find ${DESTINY} -type f -exec prename -v 's/ /./g' {} +
+# Rename accents to normal characters
+find ${DESTINY} -type f -exec prename -v 's/á/a/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/é/e/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/í/i/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/ó/o/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/ú/u/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/Á/A/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/É/E/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/Í/I/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/Ó/O/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/Ú/U/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/ñ/n/g' {} +
+find ${DESTINY} -type f -exec prename -v 's/Ñ/N/g' {} +
+# Remove other special characters
+find ${DESTINY} -type f -exec prename -v 's#[^[:alnum:]./]##g' {} +
